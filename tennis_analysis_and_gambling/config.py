a3cfce0f22b5b66ea17e1f54098ba285a9c8b183
Marc Abelé <abele.marc@gmail.com>
2024-10-10 09:35:49 +0200

    Fix ruff configuration

--- tennis_analysis_and_gambling/config.py
+++ tennis_analysis_and_gambling/config.py
@@ -1,8 +1,10 @@
-from datetime import datetime, timedelta
+from datetime import datetime
+from datetime import timedelta
 
-URL_HISTORY_ATP_FILES = "http://www.tennis-data.co.uk/alldata.php"
-ATP_FILE_XPATH = "/html/body/table[5]/tbody/tr[2]/td[3]/"
+URL_HISTORY_FILES = "http://www.tennis-data.co.uk/alldata.php"
+FILES_XPATH = "/html/body/table[5]/tbody/tr[2]/td[3]/"
 ATP_FILES_DIR = "data/external/atp"
+WTA_FILES_DIR = "data/external/wta"
 FILES_DIR = "data/"
 
 FORMAT_DATE = "%Y-%m-%d"
