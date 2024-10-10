354cd21862a7fa42ce8d7edea89d9c808ed4bd49
Marc Abelé <abele.marc@gmail.com>
2024-10-10 09:46:02 +0200

    Reorder imports and functions rework

--- tennis_analysis_and_gambling/utils.py
+++ tennis_analysis_and_gambling/utils.py
@@ -1,82 +1,77 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-import pandas as pd
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.webdriver import WebDriver
-import requests
-from os import path, makedirs, remove
-import zipfile
 from datetime import datetime
-from tennis_analysis_and_gambling.config import (
-    URL_HISTORY_ATP_FILES, 
-    ATP_FILE_XPATH, 
-    ATP_FILES_DIR,
-)
+from os import makedirs
+from os import path
+
+import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+from tennis_analysis_and_gambling.config import ATP_FILES_DIR
+from tennis_analysis_and_gambling.config import FILES_XPATH
+from tennis_analysis_and_gambling.config import URL_HISTORY_FILES
+from tennis_analysis_and_gambling.config import WTA_FILES_DIR
 
 
 def set_driver(url: str) -> WebDriver:
     chrome_options = Options()
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--remote-debugging-port=9222")
     chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--disable-site-isolation-trials")
-    chrome_options.add_argument("--disable-cookies")
-    chrome_options.add_argument("--disable-popup-blocking")
-    chrome_options.add_argument("--incognito")
-
     driver = webdriver.Chrome(options=chrome_options)
     driver.get(url)
-    return driver  
+    return driver
 
-def download_and_read_atp_file(
-    year: int, data_dir: str = ATP_FILES_DIR
-) -> pd.DataFrame:
-    driver = set_driver(url=URL_HISTORY_ATP_FILES)
-    wait = WebDriverWait(driver, 3)
+
+def save_file_from_url(file_url: str, file_name: str):
+    response = requests.get(file_url)
+    with open(file_name, "wb") as file:
+        file.write(response.content)
+    print(f"{file_name} downloaded")
+
+
+def fetch_history_file(year: int, atp_or_wta: str) -> None:
 
     current_year = datetime.now().year
-    suffix = current_year - year + 1
+
+    if atp_or_wta.lower() == "atp":
+        # xpath is built as follow: "/html/body/table[5]/tbody/tr[2]/td[3]/[_suffix_]"
+        # ATP history files are from current year (suffix: 1) to 2000
+        # WTA history files follow the last ATP history file
+        # e.g.: if current year is 2027, 2027 ATP file' suffix will be 1,
+        # 2000 ATP file' suffix will be 28
+        # and first WTA file' suffix wille be 29
+        suffix = current_year - year + 1
+        data_dir = ATP_FILES_DIR
+    elif atp_or_wta.lower() == "wta":
+        suffix = current_year - 2000
+        data_dir = WTA_FILES_DIR
+    else:
+        raise ValueError(f"{atp_or_wta} not correct. Please select 'ATP' or 'WTA'")
 
     if suffix < 1:
-        print(f"No file found for the year {year}")
-        driver.quit()
-        return pd.DataFrame()
-    dynamic_xpath = f"{ATP_FILE_XPATH}a[{suffix}]"
-    element = wait.until(EC.presence_of_element_located((By.XPATH, dynamic_xpath)))
+        raise FileNotFoundError(f"No file found for {atp_or_wta} {year}")
 
-    element_text = element.text
+    driver = set_driver(url=URL_HISTORY_FILES)
+    try:
+        wait = WebDriverWait(driver, 3)
+        dynamic_xpath = f"{FILES_XPATH}a[{suffix}]"
+        element = wait.until(EC.presence_of_element_located((By.XPATH, dynamic_xpath)))
+        element_text = element.text
 
-    if str(year) in element_text:
-        file_url = element.get_attribute("href")
-        if type(file_url) == str:
-            file_name = path.join(data_dir, f"atp_{file_url.split('/')[-1]}")
-            file_ext = path.splitext(file_name)[-1]
-            makedirs(data_dir, exist_ok=True)
-            response = requests.get(file_url)
-        with open(file_name, "wb") as file:
-            file.write(response.content)
-        print(f"{file_name} downloaded")
-
-        if file_ext == ".zip":
-            with zipfile.ZipFile(file_name, "r") as zip_ref:
-                zip_ref.extractall(ATP_FILES_DIR)
-                file = zip_ref.namelist()[0]
-            file_name = path.join(data_dir, file)
-            df = pd.read_excel(file_name)
-            remove(zip_ref.filename)
-            print(f"{zip_ref.filename} deleted")
+        if str(year) in element_text:
+            file_url = element.get_attribute("href")
+            if isinstance(file_url, str):
+                file_name = path.join(data_dir, f"{atp_or_wta.lower()}_{file_url.split('/')[-1]}")
+                makedirs(data_dir, exist_ok=True)
+                save_file_from_url(file_url=file_url, file_name=file_name)
         else:
-            df = pd.read_excel(file_name)
-        remove(file_name)
-        print(f"{file_name} deleted")
-    else:
-        print(f"No file found for the year {year}")
-        df = pd.DataFrame()
-
-    driver.quit()
-    return df
+            raise FileNotFoundError(f"No file found for {atp_or_wta} {year}")
+    finally:
+        driver.quit()
